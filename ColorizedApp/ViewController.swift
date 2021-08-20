ce7d10d989e132a5bde0c318115fe24207ea1626
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-08-21 00:09:41 +0300

    View Controller update

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -21,16 +21,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Labels
         colorFieldView.layer.cornerRadius = 20
+        
+        redIntensityLabel.text = String(format: "%.2f", redColorSlider.value)
+        greenIntensityLabel.text = String(format: "%.2f", greenColorSlider.value)
+        blueIntensityLabel.text = String(format: "%.2f", blueColorSlider.value)
+        
+        // Sliders
+        redColorSlider.minimumValue = 0
+        redColorSlider.maximumValue = 1
+        redColorSlider.minimumTrackTintColor = .red
+        
+        greenColorSlider.minimumValue = 0
+        greenColorSlider.maximumValue = 1
+        greenColorSlider.minimumTrackTintColor = .green
+        
+        blueColorSlider.minimumValue = 0
+        blueColorSlider.maximumValue = 1
+        blueColorSlider.minimumTrackTintColor = .blue
     }
     
     @IBAction func intensifyRed() {
+        redIntensityLabel.text = String(format: "%.2f", redColorSlider.value)
     }
     
     @IBAction func intensifyGreen() {
+        greenIntensityLabel.text = String(format: "%.2f", greenColorSlider.value)
     }
     
     @IBAction func intensifyBlue() {
+        blueIntensityLabel.text = String(format: "%.2f", blueColorSlider.value)
     }
     
 }
