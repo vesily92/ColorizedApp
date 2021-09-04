af0b4a783b356e29ac1bf8e0e229c0d737716400
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-09-04 13:41:38 +0300

    General update

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -19,6 +19,10 @@ class SettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
 
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,11 +32,20 @@ class SettingsViewController: UIViewController {
         greenValueLabel.text = getString(from: greenSlider)
         blueValueLabel.text = getString(from: blueSlider)
         
+        updateValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+        
+        redTextField.text = getString(from: redSlider)
+        greenTextField.text = getString(from: greenSlider)
+        blueTextField.text = getString(from: blueSlider)
+        
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate = self
+        
         redSlider.minimumTrackTintColor = .systemRed
         greenSlider.minimumTrackTintColor = .systemGreen
         
         updateColor()
-        updateValue(for: redValueLabel, greenValueLabel, blueValueLabel)
     }
     
     @IBAction func intensifyRGB(_ sender: UISlider) {
@@ -41,10 +54,13 @@ class SettingsViewController: UIViewController {
         switch sender {
         case redSlider:
             redValueLabel.text = getString(from: redSlider)
+            redTextField.text = getString(from: redSlider)
         case greenSlider:
             greenValueLabel.text = getString(from: greenSlider)
+            greenTextField.text = getString(from: greenSlider)
         default:
             blueValueLabel.text = getString(from: blueSlider)
+            blueTextField.text = getString(from: blueSlider)
         }
     }
     
@@ -73,5 +89,37 @@ class SettingsViewController: UIViewController {
             }
         }
     }
+}
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let convertibleText = Float(textField.text ?? "") else { return }
+        
+        switch textField {
+        case redTextField:
+            redSlider.value = convertibleText
+        case greenTextField:
+            greenSlider.value = convertibleText
+        default:
+            blueSlider.value = convertibleText
+        }
+        
+        updateColor()
+    }
+    
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        if textField == redTextField {
+//            greenTextField.becomeFirstResponder()
+//        } else if {
+//            textField == greenTextField {
+//                blueTextField.becomeFirstResponder()
+//            }
+//        } else {
+//            logIn()
+//            performSegue(withIdentifier: "SegueOne", sender: nil)
+//        }
+//        return true
+//    }
 }
 
